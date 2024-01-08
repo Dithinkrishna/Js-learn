0ab363fb6335fc604ab2113cf1d072c10e2d212a
dithindk123@gmail.com <dithindk123@gmail.com>
2024-01-07 23:30:15 -0800

    feat: Added javascript for sum-app

---
+++ 1-sum-app/sum-app.js
@@ -0,0 +1,6 @@
+function add(){
+    var num1=parseInt(document.getElementById("inp-text1").value);
+    var num2=parseInt(document.getElementById("inp-text2").value);
+    var sum=num1+num2;
+    document.getElementById("inp-text3").value=sum;
+}
