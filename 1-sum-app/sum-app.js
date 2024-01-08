8c7844fee4bc042361488f944b939268dce38633
dithindk123@gmail.com <dithindk123@gmail.com>
2024-01-08 12:55:54 -0800

    fix: Review comments

--- 1-sum-app/sum-app.js
+++ 1-sum-app/sum-app.js
@@ -1,6 +1,6 @@
 function add(){
-    var num1=parseInt(document.getElementById("inp-text1").value);
-    var num2=parseInt(document.getElementById("inp-text2").value);
+    var num1=parseInt(document.getElementById("inp-first-num").value);
+    var num2=parseInt(document.getElementById("inp-second-num").value);
     var sum=num1+num2;
-    document.getElementById("inp-text3").value=sum;
+    document.getElementById("output-result").value=sum;
 }
