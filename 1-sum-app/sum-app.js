b770281b300b056cd05f9e4f38a110f4a5886df4
dithindk123@gmail.com <dithindk123@gmail.com>
2024-01-10 21:28:18 -0800

    feat: Added alert box

--- 1-sum-app/sum-app.js
+++ 1-sum-app/sum-app.js
@@ -1,6 +1,12 @@
 function add() {
     var num1 = parseInt(document.getElementById("inp-first-num").value);
+    if (num1 <= 0) {
+        alert("Negative number is not supported. Please enter a positive number!")
+    }
     var num2 = parseInt(document.getElementById("inp-second-num").value);
+    if (num2 <= 0) {
+        alert("Negative number is not supported. Please enter a positive number!")
+    }
     var sum = num1 + num2;
     document.getElementById("output-result").value = sum;
 }
