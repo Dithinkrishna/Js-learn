dbf6b1a25db5a385be29904531878a192479b396
dithindk123@gmail.com <dithindk123@gmail.com>
2024-01-11 18:51:27 -0800

    feat: Added validation for negetive numbers

--- 1-sum-app/sum-app.js
+++ 1-sum-app/sum-app.js
@@ -1,11 +1,18 @@
 function add() {
     var num1 = parseInt(document.getElementById("inp-first-num").value);
+    if (num1 == null) {
+        document.getElementById("first-error-message").style.display = "none";
+    }
     if (num1 <= 0) {
-        alert("Negative number is not supported. Please enter a positive number!")
+        document.getElementById("first-error-message").style.display = "inline";
     }
     var num2 = parseInt(document.getElementById("inp-second-num").value);
+
+    if (num2 == null) {
+        document.getElementById("second-error-message").style.display = "none";
+    }
     if (num2 <= 0) {
-        alert("Negative number is not supported. Please enter a positive number!")
+        document.getElementById("second-error-message").style.display = "inline";
     }
     var sum = num1 + num2;
     document.getElementById("output-result").value = sum;
