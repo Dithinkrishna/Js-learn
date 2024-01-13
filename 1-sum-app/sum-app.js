ccf87ffe9c95d25579b94aa66c56df5c0dc7a16d
dithindk123@gmail.com <dithindk123@gmail.com>
2024-01-13 11:52:13 -0800

    feat: Added error while enter negetive inputs

--- 1-sum-app/sum-app.js
+++ 1-sum-app/sum-app.js
@@ -1,19 +1,12 @@
 function add() {
     var num1 = parseInt(document.getElementById("inp-first-num").value);
-    if (num1 == null) {
-        document.getElementById("first-error-message").style.display = "none";
-    }
-    if (num1 <= 0) {
+    var num2 = parseInt(document.getElementById("inp-second-num").value);
+    if ((num1 < 0) && (num1 < 0)) {
         document.getElementById("first-error-message").style.display = "inline";
     }
-    var num2 = parseInt(document.getElementById("inp-second-num").value);
-
-    if (num2 == null) {
-        document.getElementById("second-error-message").style.display = "none";
-    }
-    if (num2 <= 0) {
+    if ((num2 < 0) && (num2 < 0)) {
         document.getElementById("second-error-message").style.display = "inline";
     }
-    var sum = num1 + num2;
+    sum = num1 + num2;
     document.getElementById("output-result").value = sum;
 }
