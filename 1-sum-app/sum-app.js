3bf1bff50e440bc0de9243ef6c5909687782977a
dithindk123@gmail.com <dithindk123@gmail.com>
2024-01-13 20:06:20 -0800

    feat: Updated js file

--- 1-sum-app/sum-app.js
+++ 1-sum-app/sum-app.js
@@ -1,11 +1,15 @@
 function add() {
     var num1 = parseInt(document.getElementById("inp-first-num").value);
     var num2 = parseInt(document.getElementById("inp-second-num").value);
-    if ((num1 < 0) && (num1 < 0)) {
+    if (num1 < 0 && num1 < 0) {
         document.getElementById("first-error-message").style.display = "inline";
+    } else {
+        document.getElementById("first-error-message").style.display = "none";
     }
-    if ((num2 < 0) && (num2 < 0)) {
+    if (num2 < 0 && num2 < 0) {
         document.getElementById("second-error-message").style.display = "inline";
+    } else {
+        document.getElementById("second-error-message").style.display = "none";
     }
     sum = num1 + num2;
     document.getElementById("output-result").value = sum;
