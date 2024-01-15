92fb924860e5d5318641b0d76e889eed828080ae
dithindk123@gmail.com <dithindk123@gmail.com>
2024-01-15 00:19:58 -0800

    feat: Added error message in sum-app

--- 1-sum-app/sum-app.js
+++ 1-sum-app/sum-app.js
@@ -1,16 +1,24 @@
 function add() {
     var num1 = parseInt(document.getElementById("inp-first-num").value);
     var num2 = parseInt(document.getElementById("inp-second-num").value);
-    if (num1 < 0 && num1 < 0) {
+    var hasError = false;
+    if (num1 && num1 < 0) {
         document.getElementById("first-error-message").style.display = "inline";
+        hasError = true;
     } else {
         document.getElementById("first-error-message").style.display = "none";
     }
-    if (num2 < 0 && num2 < 0) {
+    if (num2 && num2 < 0) {
         document.getElementById("second-error-message").style.display = "inline";
+        hasError = true;
     } else {
         document.getElementById("second-error-message").style.display = "none";
     }
-    sum = num1 + num2;
+    if (hasError) {
+        document.getElementById("output-result").value = "";
+        return;
+    }
+
+    const sum = num1 + num2;
     document.getElementById("output-result").value = sum;
 }
