233573ea8997702783b100b59c67604c80584292
DESKTOP-5EOJFI2\Dithu <dithindk123@gmail.com>
2024-02-11 17:59:22 -0800

    no message

--- 2-datatypes/boolean.js
+++ 2-datatypes/boolean.js
@@ -4,8 +4,8 @@ prompt.start();
 
 prompt.get(["number1", "number2"], main)
 function main(err, res) {
-    const number1 = Boolean(res.number1);
-    const number2 = Boolean(res.number2);
+    const number1 = true(res.number1);
+    const number2 = false(res.number2);
     const boolean = res.number1 >= res.number2
     console.log(boolean)
 }
