d3d6518852584467af28d5eb98102ee8c0ce0e76
Dithinkrishna <dithindk123@gmail.com>
2024-03-15 19:29:42 +0530

    feat: Updated reviewed codes

--- 2-datatypes/ifelse.js
+++ 2-datatypes/ifelse.js
@@ -4,8 +4,8 @@ prompt.start();
 
 prompt.get(["number1"], main)
 function main(err, res) {
-    let number1 = Number(res.number1);
-    if (res.number1 >= 10 && res.number1 <= 20) {
+    const number1 = Number(res.number1);
+    if (number1 >= 10 && number1 <= 20) {
         console.log("Number is between 10 and 20");
     } else {
         console.log("Number is not in between 10 and 20")
