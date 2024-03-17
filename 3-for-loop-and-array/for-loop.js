8d4cb890cfb41a8f228c44885603fcc2b9409752
Dithinkrishna <dithindk123@gmail.com>
2024-03-17 21:41:57 +0530

    feat: Updated new task

---
+++ 3-for-loop-and-array/for-loop.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt");
+prompt.start();
+prompt.get(["input"], main)
+function main(err, res) {
+    const names = ["Dithin", "Dinu", "Priji", "Arch"];
+    const input = (res.input);
+    for (let index = 0; index < names.length; index++) {
+        if (names[index].toUpperCase().includes(input.toUpperCase())) {
+            console.log(`Character ${input} is present in ${names[index]}`)
+            continue;
+        }
+        console.log(`Character ${input} is not present in ${names[index]}`)
+    }
+    return;
+}
+
