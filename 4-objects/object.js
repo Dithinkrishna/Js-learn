094d73a1f7a277ed7a208d0766a2d38b902f1153
Dithinkrishna <dithindk123@gmail.com>
2024-03-29 23:29:06 +0530

    feat: Initial updates

---
+++ 4-objects/object.js
@@ -0,0 +1,10 @@
+const stevesBook = {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+}
+function getBookDetails() {
+    console.log("-----------------------------------------------------------------");
+    console.log(`The book ${stevesBook.title} written by ${stevesBook.author} `);
+    console.log("------------------------------------------------------------------");
+}
+getBookDetails();
