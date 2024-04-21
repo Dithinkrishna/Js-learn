2623eaf2cfb0a01a4e337eafc75bdea1b0546185
Dithinkrishna <dithindk123@gmail.com>
2024-04-21 16:15:56 +0530

    feat: Secondary update

--- 5-html-js/menucard.js
+++ 5-html-js/menucard.js
@@ -1,10 +1,15 @@
-function add() {
-    const foodName = document.createElement("input-food");
-    foodName.setAttribute("type", "text");
-    foodName.setAttribute("value", "Porotta");
-    document.getElementById("body").appendChild(foodName);
+function addElement() {
+    const foodName = document.createElement(`input`);
+    foodName.style.cssText = "margin-top:15px"
+    const deleteButton = document.createElement(`button`);
+    deleteButton.style.cssText = "width:30px;height:23px;margin-top:15px;color:red;border-color:grey;"
+    foodName.value = "";
+    deleteButton.textContent = " - ";
+    const name = document.getElementById("menu-card");
+    name.appendChild(foodName);
+    name.appendChild(deleteButton);
 }
-function readOnly() {
-    document.getElementById('input-tea').readOnly = true;
+function toggleReadOnly() {
+    const currentInputReadOnly = document.getElementById("input-tea").readOnly;
+    document.getElementById('input-tea').readOnly = !currentInputReadOnly;
 }
-add()
